a3b8c83d56ca8215376c3f416b859f0c57fe8702
vika lo <victoria.tchekushkina@yandex.ru>
2022-09-15 20:04:35 +0400

    done

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+function CheckInput(num) {
+    let input = document.getElementById("input" + num);
+    if (input.value === '' || input.value.includes('e')) {
+        input.classList += ' errorClass';
+        return false;
+    }
+    else {
+        input.classList.value = input.classList.value.replaceAll(' errorClass', '');
+        return true;
+    }
+}
+
 function Calc() {
+    if (!CheckInput(1) || !CheckInput(2)) {
+        return;
+    }
     let value1, value2, selectValue;
     value1 = parseFloat(document.getElementById("input1").value);
     value2 = parseFloat(document.getElementById("input2").value);
@@ -15,7 +30,11 @@ function Calc() {
             result = value1 * value2;
             break;
         case '/':
-            result = value1/value2;
+            if (value2 == 0) {
+                document.getElementById("input2").classList += ' errorClass';
+                return;
+            }
+            result = value1 / value2;
         default:
             break;
     }
@@ -23,5 +42,4 @@ function Calc() {
     let result2 = document.getElementById('result2');
     result1.innerHTML = result2.innerHTML;
     result2.innerHTML = value1 + ' ' + selectValue + ' ' + value2 + ' = ' + result;
-    //console.log(value1, value2, selectValue, parseFloat(value1) - parseFloat(value2), document.getElementById("selectId").value)
 }
